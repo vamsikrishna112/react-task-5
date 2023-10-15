57ec334398b111d5d5b482bf265f9197a8904c12
vamsikrishna112 <143983753+vamsikrishna112@users.noreply.github.com>
2023-10-15 14:49:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Age from './agecalculator';
+
+function App() {
+  return (
+    <div>
+      <Age/>
+    </div>
+  );
+}
+
+export default App;
